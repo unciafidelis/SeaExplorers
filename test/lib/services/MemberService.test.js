cbd3b09e597e31b817cd751c2e7bb4c78336c401
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-05-14 23:27:58 -0500

    Update: MemberServiceTest

--- test/lib/services/MemberService.test.js
+++ test/lib/services/MemberService.test.js
@@ -1,7 +1,8 @@
 const MemberService = require('../../../lib/services/MemberService')
 
 describe('Unit test for Member Service', () => {
-    test('Should return a bigger value of Member List Length', () => {
-        expect(MemberService.getAllMembers().length > 0).toBe(true)
-    } )
+    test('Should return a bigger value of Member List Length', async () => {
+        const allMembers = await MemberService.getAllMembers()
+        expect(allMembers.length > 0).toBe(true)
+    })
 })
