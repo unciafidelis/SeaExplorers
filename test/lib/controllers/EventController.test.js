f57a0bf1161ddf8d2ea10f2eb0eccbef9891d4c0
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-05-15 13:50:24 -0500

    Update: EventControllerTest(Adding getEvent Test)

--- test/lib/controllers/EventController.test.js
+++ test/lib/controllers/EventController.test.js
@@ -1,9 +1,12 @@
 const EventController = require('../../../lib/controllers/EventController')
 
 describe('Unit test for Event Controller', () => {
-    test('Should return member list length', async () => {
+    test('Should return event list length', async () => {
         const events = await EventController.getAllEvents();
         expect(events.length > 0).toBe(true);
     });  
-
+    test('Should return name of event', async () => {
+        const events = await EventController.getEvent(1);
+        expect(events.name).toBe("Ataque Pirata");
+    });
 });
