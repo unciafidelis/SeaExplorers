b5e9b679f129d76aa351727ee0792948f7b5b1ad
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-05-15 01:06:18 -0500

    Update: OrganizerServiceTest(Adding getMember Test)

--- test/lib/services/OrganizerService.test.js
+++ test/lib/services/OrganizerService.test.js
@@ -1,8 +1,12 @@
-const MemberService = require('../../../lib/services/MemberService')
+const OrganizerService = require('../../../lib/services/OrganizerService')
 
 describe('Unit test for Organizer Service', () => {
     test('Should return a bigger value of Organizer List Length', async () => {
-        const allMembers = await MemberService.getAllMembers()
-        expect(allMembers.length > 0).toBe(true)
+        const allOrganizers = await OrganizerService.getAllMembers()
+        expect(allOrganizers.length > 0).toBe(true)
+    })
+    test('Should return a name of the specific Organizer', async () => {
+        const organizer = await OrganizerService.getMember(1)
+        expect(organizer.name).toBe("dalex")
     })
 })
