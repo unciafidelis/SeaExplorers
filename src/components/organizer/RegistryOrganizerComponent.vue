3017ee1a0f794de70bd10efcf118f35590a9d1ab
MagdielAndres <magdiel.vampire@gmail.com>
2022-05-15 11:04:41 -0500

    Update: Actualizando el formulario de RegsitryOrganization

--- src/components/organizer/RegistryOrganizerComponent.vue
+++ src/components/organizer/RegistryOrganizerComponent.vue
@@ -12,58 +12,46 @@
           <v-text-field
             ref="name"
             v-model="name"
-            :rules="[() => !!name || 'This field is required']"
+            :rules="[() => !!name || 'No deje el campo vacío']"
             :error-messages="errorMessages"
-            label="Full Name"
-            placeholder="John Doe"
+            label="Nombre"
+            placeholder="Escriba su nombre"
             required
           ></v-text-field>
           <v-text-field
-            ref="address"
-            v-model="address"
-            :rules="[
-              () => !!address || 'This field is required',
-              () => !!address && address.length <= 25 || 'Address must be less than 25 characters',
-              addressCheck
-            ]"
-            label="Address Line"
-            placeholder="Snowy Rock Pl"
-            counter="25"
+            ref="username"
+            v-model="username"
+            :rules="[() => !!username || 'No deje el campo vacío']"
+            :error-messages="errorMessages"
+            label="Usuario"
+            placeholder="Escriba su usuario"
             required
           ></v-text-field>
           <v-text-field
-            ref="city"
-            v-model="city"
-            :rules="[() => !!city || 'This field is required', addressCheck]"
-            label="City"
-            placeholder="El Paso"
+            ref="email"
+            v-model="email"
+            :rules="[() => !!username || 'No deje el campo vacío']"
+            label="Email"
+            placeholder="Escriba su email"
             required
           ></v-text-field>
+          
           <v-text-field
-            ref="state"
-            v-model="state"
-            :rules="[() => !!state || 'This field is required']"
-            label="State/Province/Region"
+            ref="password"
+            v-model="password"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min, () => !!password || 'No deje vacío el campo']"
+            :error-messages="errorMessages"
+            :type="show1 ? 'text' : 'password'"
+            name="password"
+            label="Contraseña"
+            placeholder="Escriba la contraseña"
+            hint="Al menos 8 carácteres"
+            counter
             required
-            placeholder="TX"
+            @click:append="show1 = !show1"
           ></v-text-field>
-          <v-text-field
-            ref="zip"
-            v-model="zip"
-            :rules="[() => !!zip || 'This field is required']"
-            label="ZIP / Postal Code"
-            required
-            placeholder="79938"
-          ></v-text-field>
-          <v-autocomplete
-            ref="country"
-            v-model="country"
-            :rules="[() => !!country || 'This field is required']"
-            :items="countries"
-            label="Country"
-            placeholder="Select..."
-            required
-          ></v-autocomplete>
+          
         </v-card-text>
         <v-divider class="mt-12"></v-divider>
         <v-card-actions>
@@ -95,7 +83,7 @@
             text
             @click="submit"
           >
-            Submit
+            Registrar
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -106,21 +94,33 @@
 <script>
   export default {
     data: () => ({
-      countries: ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua &amp; Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia &amp; Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde', 'Cayman Islands', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D Ivoire', 'Croatia', 'Cruise Ship', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait', 'Kyrgyz Republic', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia', 'Rwanda', 'Saint Pierre &amp; Miquelon', 'Samoa', 'San Marino', 'Satellite', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St Kitts &amp; Nevis', 'St Lucia', 'St Vincent', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', `Timor L'Este`, 'Togo', 'Tonga', 'Trinidad &amp; Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks &amp; Caicos', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Venezuela', 'Vietnam', 'Virgin Islands (US)', 'Yemen', 'Zambia', 'Zimbabwe'],
       errorMessages: '',
       name: null,
+      username: null,
+      email: null,
       address: null,
-      city: null,
-      state: null,
-      zip: null,
-      country: null,
       formHasErrors: false,
-    }),
+    },
+    {
+        show1: false,
+        show2: true,
+        show3: false,
+        show4: false,
+        password: '',
+        rules: {
+          required: value => !!value || 'Contraseña requerido.',
+          min: v => v.length >= 8 || 'Minimo 8 caracteres',
+          emailMatch: () => (`The email and password you entered don't match`),
+        },
+      }
+    ),
 
     computed: {
       form () {
         return {
           name: this.name,
+          username: this.username,
+          email: this.email,
           address: this.address,
           city: this.city,
           state: this.state,
