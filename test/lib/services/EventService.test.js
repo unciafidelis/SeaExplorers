0311e554215133832d1526e88cdff67f30d309f7
DianaMagallanes <dianakarina2003@example.com>
2022-05-14 23:23:27 -0700

    Update: EventService y EventService.test (se agrega metodo newEvent con su respectiva prueba)

--- test/lib/services/EventService.test.js
+++ test/lib/services/EventService.test.js
@@ -9,5 +9,16 @@ describe('Unit test for Event Service', () => {
         const events = await EventService.getEvent(1);
         expect(events.name).toBe("Ataque Pirata");
     });
+    
+    test('Should return name of added event', async () => {
+        const events = await EventService.newEvent(
+        {"name": "Un test service",
+        "activity": "test act",
+        "location": "test location",
+        "type": "playero",
+        "personQuota": 10,
+        "organizer": "Juanito Perez"});
+        expect(events.name).toBe("Un test service");
+    });
 
 });
