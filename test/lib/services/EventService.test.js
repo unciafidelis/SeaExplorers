eda16d413c6d6ab32d541305e4d8835b42933aa0
DianaMagallanes <dianakarina2003@example.com>
2022-05-14 23:22:19 -0700

    Update: EventService y EventService.test (se agrega metodo get unico con su respectiva prueba)

--- test/lib/services/EventService.test.js
+++ test/lib/services/EventService.test.js
@@ -5,5 +5,9 @@ describe('Unit test for Event Service', () => {
         const events = await EventService.getAllEvents();
         expect(events.length > 0).toBe(true);
     });
-    
+    test('Should return name of event', async () => {
+        const events = await EventService.getEvent(1);
+        expect(events.name).toBe("Ataque Pirata");
+    });
+
 });
