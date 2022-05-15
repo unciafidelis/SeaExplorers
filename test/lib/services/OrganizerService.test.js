a5d7c96c6f1d4c2905262264c7cb4ed2f5a15488
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-05-15 01:13:51 -0500

    Update: OrganizerServiceTest(Adding newOrganizer Test)

--- test/lib/services/OrganizerService.test.js
+++ test/lib/services/OrganizerService.test.js
@@ -2,11 +2,21 @@ const OrganizerService = require('../../../lib/services/OrganizerService')
 
 describe('Unit test for Organizer Service', () => {
     test('Should return a bigger value of Organizer List Length', async () => {
-        const allOrganizers = await OrganizerService.getAllMembers()
+        const allOrganizers = await OrganizerService.getAllOrganizers()
         expect(allOrganizers.length > 0).toBe(true)
     })
     test('Should return a name of the specific Organizer', async () => {
-        const organizer = await OrganizerService.getMember(1)
+        const organizer = await OrganizerService.getOrganizer(1)
         expect(organizer.name).toBe("dalex")
+    })
+    test('Should insert a new organizer', async () => {
+        const data = {
+            name: "TestOrganizer",
+            username: "testorganizer",
+            email: "test@test.com",
+            password: "123123"
+        }
+        const newOrganizer = await OrganizerService.newOrganizer(data)
+        expect(newOrganizer.name).toBe("TestOrganizer")
     })
 })
