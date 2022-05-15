52ea0d8625f4dcb33d6ea751a985a9d3e3e6d828
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-05-14 23:52:24 -0500

    Update: MemberServiceTest(Adding test for getMember)

--- test/lib/services/MemberService.test.js
+++ test/lib/services/MemberService.test.js
@@ -5,4 +5,8 @@ describe('Unit test for Member Service', () => {
         const allMembers = await MemberService.getAllMembers()
         expect(allMembers.length > 0).toBe(true)
     })
+    test('Should return a name of the specific Member', async () => {
+        const member = await MemberService.getMember(1)
+        expect(member.name).toBe("DianaMagallanes")
+    })
 })
