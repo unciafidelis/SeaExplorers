736c2e4482ef6c55f9d764ba8f97b06f01315f4d
MagdielAndres <magdiel.vampire@gmail.com>
2022-05-15 20:30:40 -0500

    Add: Agregando el la lista del EventComponent

---
+++ src/components/event/EventComponent.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="list row">
+    <div class="col-md-6">
+      <h4> Lista de eventos </h4>
+      <ul class="list-group">
+        <li class="list-group-item"
+          :class="{ active: index == currentIndex }"
+          v-for="(event, index) in events"
+          :key="index"
+          @click="setActiveExplorer(event, index)"
+        >
+          {{ event.username }}
+        </li>
+
+      </ul>
+    </div>
+    <div class="col-md-6">
+      <div v-if="currentExplorer">
+        <h4>Eventos</h4>
+        <div>
+          <br><label><strong>Nombre:</strong></label> {{ currentExplorer.name}}
+          <br><label><strong>Actividad:</strong></label> {{ currentExplorer.activity}}
+          <br><label><strong>Locazión:</strong></label> {{ currentExplorer.location}}
+          <br><label><strong>Tipo:</strong></label> {{ currentExplorer.type}}
+          <br><label><strong>Cupo de persona:</strong></label> {{ currentExplorer.personQuota}}
+          <br><label><strong>Organizador:</strong></label> {{ currentExplorer.organizer}}
+          <br><label><strong>Solo miembros:</strong></label> {{ currentExplorer.onlyMembers}}
+
+        </div>
+        <router-link :to="'/event/' + currentExplorer.id" class="btn btn-info"> Editar</router-link>
+      </div>
+      <div v-else>
+        <br />
+        <p> Selecciona un evento.</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import EventService from "../../../lib/services/EventService";
+export default {
+  name: "event-list",
+  data() {
+    return {
+      tutorials: [],
+      events: [],
+      currentTutorial: null,
+      currentExplorer: null,
+      currentIndex: -1,
+      title: "",
+      explorerId: ""
+    };
+  },
+  methods: {
+    getAllExplorers(){
+      EventService.getAll()
+        .then(response => {
+          this.events = response.data;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    setActiveExplorer(event, index) {
+      this.currentExplorer= event;
+      this.currentIndex = event? index : -1;
+    }
+  },
+  mounted() {
+    this.getAllExplorers();
+  }
+};
+</script>
